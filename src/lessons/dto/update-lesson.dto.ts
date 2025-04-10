746eb8a31859a9e6297dcb998ebbeaeecae37590
Alisher Sharipov <alishersharipov670@gmail.com>
2025-04-10 17:44:49 +0500

    data base url changed

--- src/lessons/dto/update-lesson.dto.ts
+++ src/lessons/dto/update-lesson.dto.ts
@@ -1,24 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import {
-  IsDate,
-  IsMongoId,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsDate, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateLessonDto {
+  @ApiProperty({ example: '2025-04-09' })
   @IsOptional()
   @IsDate()
   @Type(() => Date)
   @IsNotEmpty()
   date?: string;
 
+  @ApiProperty({ example: '08:00' })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
   startTime?: string;
 
+  @ApiProperty({ example: '11:30' })
   @IsOptional()
   @IsString()
   @IsNotEmpty()
